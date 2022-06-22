348eab78d2136780577c01834325d3ab8c354f43
mikespin <michalkrecisz@yahoo.com>
2022-06-22 21:11:06 +0200

    init exp repo

---
+++ src/exp1/exp1.js
@@ -0,0 +1,15 @@
+import * as THREE from "three";
+
+/*
+    
+*/
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, 800 / 800, 0, 10);
+
+console.log("scene", scene);
+
+class SceneManager {
+  constructior({ canvasWidth, canvasHeignt }) {}
+}
+
